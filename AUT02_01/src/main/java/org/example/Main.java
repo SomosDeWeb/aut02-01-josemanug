3fb4d81ac6f6bd1ca707ad0cb510171a6921b9e2
josemanug <josemanu1711@hotmail.com>
2025-10-06 12:28:16 +0100

    Creación de la clase principal qie contiene el método Main

--- AUT02_01/src/main/java/org/example/Main.java
+++ AUT02_01/src/main/java/org/example/Main.java
@@ -4,14 +4,44 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+
+            System.out.println("Seleccione una opción");
+            int opcion = sc.nextInt();
+            sc.nextLine();
+            System.out.println("""
+                === Gestor de Estudiantes ===
+                1. Añadir estudiante
+                2. Listar estudiantes
+                3. Buscar por nombre
+                4. Calcular nota media general
+                5. Mostrar mejor estudiante
+                6. Salir
+                """);
+
+            switch (opcion) {
+                case 1:
+                    funcion;
+                    break;
+                case 2:
+                    funcion;
+                    break;
+                case 3:
+                    funcion;
+                    break;
+                case 4:
+                    funcion;
+                    break;
+                case 5:
+                    funcion;
+                    break;
+                case *:
+                    break;
+            }
+        } while(opcion != 6)
+
+
     }
 }
